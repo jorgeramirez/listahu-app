1dd1dbab39441cfcf784a9075138137cc9659870
Jorge Ramírez <jorgeramirez1990@gmail.com>
2015-09-04 23:53:28 -0400

    Added pagination and container height fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,19 +10,48 @@
 angular.module('ListaHuApp')
   .controller('MainCtrl', function ($scope, ComplaintsService) {
 
-    var complaints = ComplaintsService.query();
-    $scope.complaints = complaints;
+    $scope.complaints = [];
+    var meta = {};
+
+    function loadComplaints(page) {
+      
+      ComplaintsService.get(page).then(function(complaints) {
+        $scope.complaints = $scope.complaints.concat(complaints.results);
+        meta = {
+          total: complaints.results,
+          next: complaints.next,
+          previous: complaints.previous
+        };
+      });
+    }
+
+    loadComplaints(1);
+
+    $scope.loadNextPage = function() {
+      
+      if(!meta.next) {
+        return;
+      }
+      var page = parseInt(meta.next.split('?page=')[1], 10);
+      loadComplaints(page);
+    };
 
     function fixWidth(items) {
       // we ensure that all element have the same width
-      var maxWidth = "", w;
+      var maxWidth = '', w;
 
       for(var i = 0; i < items.length; i++) {
         w = $(items[i]).css('width');
-        maxWidth = maxWidth < w ? w : maxWidth; 
+        maxWidth = maxWidth < w ? w : maxWidth;
       }
-      maxWidth = parseInt(maxWidth.substr(0, maxWidth.search('px')));
+      maxWidth = parseInt(maxWidth.substr(0, maxWidth.search('px')), 10);
       $('.item .pin').css('width', (maxWidth - 4) + 'px');
+
+      // also fix frame height based on last item
+      var lastItem = $(items[items.length - 1]);
+      var top = parseInt(lastItem.css('top').replace('px', ''), 10);
+      var height = parseInt(lastItem.css('height').replace('px', ''), 10);
+      $('.frame').css('height', (top + height) + 'px');
     }
 
     $scope.$on('ngRepeatAfterRender', function() {
--- app/scripts/services/complaints.js
+++ app/scripts/services/complaints.js
@@ -12,15 +12,10 @@ angular.module('ListaHuApp').factory('ComplaintsService',
 
   function ($resource, Config) {
 
-      var ComplaintsService = $resource(Config.API_URL + '/denuncias/', {}, {
-        query: {
-          method: 'GET', 
-          isArray: true, 
-          transformResponse: function(response) {
-            var data = angular.fromJson(response);
-            return data.results;
-          }
+      var Complaints = $resource(Config.API_URL + '/denuncias/');
+      return {
+        get: function(page) {
+          return Complaints.get({page: page}).$promise;
         }
-      });
-      return ComplaintsService;
+      };
   });
